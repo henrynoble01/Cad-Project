2852a29ae94d56b619d118622f9bfe786cca7193
henrynoble01 <henrynoble01@gmail.com>
2021-07-02 10:48:27 +0100

    chore: adding the click functionality for the modal

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 "use strict"
 
 let sidebar = document.querySelectorAll('.side-list')
-console.log(sidebar);
+let showGrid = document.querySelectorAll('.side-list-item')
+// ************************************//
+let angleIcon = document.querySelector('.angle-left')
+let head = document.querySelectorAll('.head')
+let sideNav = document.querySelectorAll('.side-nav')
+let listText = document.querySelectorAll('.list-text')
+let userDevice = document.querySelector('.modal-device-display')
+
+
+
+console.log(angleIcon);
+showGrid.forEach(element =>  {
+    element.addEventListener('click', () => {
+        angleIcon.classList.toggle('hidden');
+        console.log(element);
+        // 
+        head.forEach(item => {
+            item.classList.toggle('hidden');
+        })
+        // 
+        sideNav.forEach(item => {
+            item.classList.toggle('hidden');
+        })
+        // 
+        listText.forEach(item => {
+            item.classList.toggle('hidden');
+        })
+
+        userDevice.classList.toggle('show-modal');
+    })
+} )
